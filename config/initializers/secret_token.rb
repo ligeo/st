6aebc6b44d40cf8cfe5bbc5ec7a203b334f3b254
Ligeo <ligeo@yandex.ru>
2013-02-13 08:47:22 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+St::Application.config.secret_token = 'fbd343784eee63eff5cb5799eaef3a92ce448d4f20f7e78e573dc63b5ff257826e01bcf92efb7ab6bc418eb8f8be60ae0774b580766635acc745bc6004c2aa4e'
